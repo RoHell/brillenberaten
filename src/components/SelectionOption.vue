dfa7b30fa0060083a85d374ce1f02314957be408
DOCFLOW\karol.makowski <karol.makowski@docflow.pl>
2018-10-08 09:41:04 +0200

    png to base64

--- src/components/SelectionOption.vue
+++ src/components/SelectionOption.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .option
-    img(:src="option.image")
+    img(:src="`data:image/png;base64, ${option.image}`")
     .option__info
       checkbox(@checked="onCheck")
       .option__info__description
@@ -9,10 +9,8 @@
           .type__text {{ option.type }}
           svg(
             xmlns='http://www.w3.org/2000/svg',
-            xmlns:xlink='http://www.w3.org/1999/xlink',
             version='1.1',
-            viewBox='0 0 24 24'
-            :title="option.info")
+            viewBox='0 0 24 24')
             path(
               d='M13,9H11V7H13M13,17H11V11H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z')
 </template>
